1bc06526e3f6c50236a5459f8c8d047fd3e8a76d
adithyasn11 <adithyasn2487@gmail.com>
2025-04-30 10:03:46 +0530

    chore: remove next.config.js as it is no longer needed

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['your-image-domain.com'], // Add domains where your images are hosted
-    unoptimized: false,
-  },
-}
-
-module.exports = nextConfig
